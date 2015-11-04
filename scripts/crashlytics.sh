af653a91daf5d460650da23d7c2560ad692a7010
d <watanabe1205@gmail.com>
2015-11-05 00:47:05 +0900

    微修正

--- scripts/crashlytics.sh
+++ scripts/crashlytics.sh
@@ -2,5 +2,5 @@
 
 ./Crashlytics.framework/submit $API_KEY $BUILD_SECRET \
   -ipaPath ./build/UIImageEffects.ipa \
-  -emails watanabe1205@gmail.com﻿
+  -emails watanabe1205@gmail.com﻿ \
   -notifications YES
