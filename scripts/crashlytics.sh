157587c522c6216f53c2a26c041c499fbbc83cee
d <watanabe1205@gmail.com>
2015-11-05 23:43:01 +0900

    試行錯誤

--- scripts/crashlytics.sh
+++ scripts/crashlytics.sh
@@ -2,6 +2,4 @@
 
 ./Crashlytics.framework/submit $API_KEY $BUILD_SECRET \
   -ipaPath ./build/UIImageEffects.ipa \
-  -emails watanabe1205@gmail.com﻿ \
-  -groupAliases TestGroup \
   -notifications YES
