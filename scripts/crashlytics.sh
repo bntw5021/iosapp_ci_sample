154d432f3ac730f9b8ab16846296700b41a08d29
d <watanabe1205@gmail.com>
2015-11-05 22:40:19 +0900

    試行錯誤 -> submit Crashlytics: Failed to add testers/groups to the release

--- scripts/crashlytics.sh
+++ scripts/crashlytics.sh
@@ -2,7 +2,5 @@
 
 ./Crashlytics.framework/submit $API_KEY $BUILD_SECRET \
   -ipaPath ./build/UIImageEffects.ipa \
-  -emails watanabe1205@gmail.com﻿ \
-  -groupAliases TestGroup \
   -notifications YES \
   -debug YES
